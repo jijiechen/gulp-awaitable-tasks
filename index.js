06cfa709ea90c66dc49befe2b34277f520c9b289
Jijie Chen <jijie.chen@someplace.com>
2016-03-29 00:03:23 +0800

    Added gulp-awaitable-tasks implemention

---
+++ index.js
@@ -0,0 +1,57 @@
+
+// gulp  use...
+var gulpEngine;
+
+function defineTask(name, dependencies, taskFn){
+    if(arguments.length < 3 && typeof dependencies === 'function'){
+        taskFn = dependencies;
+        dependencies = [];
+    }
+
+    if(!taskFn){
+        gulpEngine.task(name, dependencies);
+    }else if(taskFn.constructor.name === 'GeneratorFunction'){
+        gulpEngine.task(name, dependencies, function (callback) {
+            arrangeAsyncSteps(taskFn(), callback);
+        });
+    }else {
+        gulpEngine.task(name, dependencies, taskFn);
+    }
+
+    function arrangeAsyncSteps(gen, cb){
+        var context = gen.next();
+        if(context.done){ cb(); return; }
+
+        var asyncStep = context.value;
+        if (asyncStep && typeof asyncStep.pipe === 'function'){
+            // consume and wait for completion of a stream: https://github.com/robrich/orchestrator/blob/master/lib/runTask.js
+            onEndOfStream(asyncStep, { error: true, readable: asyncStep.readable, writable: asyncStep.writable && !asyncStep.readable }, function(err){
+                if(err){ cb(err); return; }
+                arrangeAsyncSteps(gen, cb);
+            });
+            consumeStream(asyncStep);
+        }else if (asyncStep && typeof asyncStep.then === 'function'){
+            // wait for promise to resolve
+            asyncStep.then(function () {
+                arrangeAsyncSteps(gen, cb);
+            }, function(err) {
+                cb(err);
+            });
+        }
+    }
+}
+
+
+module.exports = function (gulp) {
+    gulpEngine = gulp;
+    if(!gulpEngine){
+        gulpEngine = require('gulp');
+    }
+
+    return defineTask;
+};
+
+
+
+
+
